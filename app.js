32207e1e8eb06bd1ee1cfb5a7d9b6ae82c1c1658
dipangshu22 <dipangshu22@gmail.com>
2025-02-15 21:00:13 +0530

    index with ejs

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ app.use(express.static(__dirname+'/public'))
 //ejs file path
 app.set('views','./src/views')
 //view engine
-app.use('view engine','ejs')
+app.set('view engine','ejs')
 app.use('/category',categoryRouter)
 app.use('/product',productRouter)
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.listen(port,(err)=>{
     if(err) throw err
