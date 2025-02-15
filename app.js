a12d2cf2545935635b347df4616c260930f8aebc
dipangshu22 <dipangshu22@gmail.com>
2025-02-15 20:46:01 +0530

    path

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ let port=8810
 
 let categoryRouter=require('./src/controller/categoryRouter')()
 let productRouter=require('./src/controller/productRouter')()
-
+//static file path 
+app.use(express.static(__dirname+'/public'))
+//ejs file path
+app.set('views','./src/views')
+//view engine
+app.use('view engine','ejs')
 app.use('/category',categoryRouter)
 app.use('/product',productRouter)
 
